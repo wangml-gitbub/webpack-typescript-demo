a50521ed429fc05a89b0195963ed9a0c58521f4a
wangmeiling <wangmeiling@xmfunny.com>
2023-05-11 15:50:01 +0800

    <misc> index.ts test.ts 文件

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ let res = _.padStart('Hello TypeScript!', 20, 'a')
 
 let arr: number[] = [1, 2]
 
-arr.map(item => item+1)
-console.log(res)
-console.log(arr)
+let resArr = arr.map(item => item = item+1)
+console.log(res) // aaaHello TypeScript!
+console.log(arr, resArr) // [ 1, 2 ] [ 2, 3 ]
+
+import { add } from './test'
+console.log('add', add(2, 3)) // add 5
---
+++ src/test.ts
@@ -0,0 +1,7 @@
+
+export function add(x: number, y: number): number{
+  return x + y
+}
+
+console.log(add(1, 2)) // 3
+
